3fa5bc63151ac9009ce5710787819228373cf745
John Blum <jblum@pivotal.io>
2017-05-18 17:19:10 -0700

    Introduce the o.c.d.core.model.Name class to model a person's name consisting of first, middle and last names.

---
+++ src/main/java/org/cp/domain/core/model/Name.java
@@ -0,0 +1,296 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.domain.core.model;
+
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalArgumentException;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.Nameable;
+import org.cp.elements.lang.ObjectUtils;
+import org.cp.elements.lang.StringUtils;
+import org.cp.elements.lang.annotation.Immutable;
+import org.cp.elements.util.ComparatorResultBuilder;
+
+/**
+ * Abstract Data Type (ADT) modeling a name containing a first name, last name and middle name (or initial(s)).
+ *
+ * This type is immutable.
+ *
+ * @author John Blum
+ * @see java.io.Serializable
+ * @see java.lang.Comparable
+ * @see org.cp.elements.lang.Nameable
+ * @see org.cp.elements.lang.annotation.Immutable
+ * @since 1.0.0
+ */
+@Immutable
+public final class Name implements Comparable<Name>, Nameable<Name>, Serializable {
+
+  public static final String NAME_PART_SEPARATOR = StringUtils.SINGLE_SPACE;
+
+  protected static final String DEFAULT_MIDDLE_NAME = null;
+
+  private static final long serialVersionUID = 83448823861933031L;
+
+  private final String firstName;
+  private final String lastName;
+  private final String middleName;
+
+  /**
+   * Factory method used to construct a new instance of {@link Name} of the given {@link Nameable}.
+   *
+   * @param nameable {@link Nameable} object used to construct and form this {@link Name}.
+   * @return a new instance of {@link Name} initialized with the given {@link Nameable} object.
+   * @throws IllegalArgumentException if {@link Nameable} is {@literal null}.
+   * @see org.cp.elements.lang.Nameable#getName()
+   * @see org.cp.domain.core.model.Name
+   * @see #of(String, String, String)
+   */
+  public static Name of(Nameable<Name> nameable) {
+    return Optional.ofNullable(nameable)
+      .map(localNameable -> {
+        Name name = localNameable.getName();
+        return of(name.getFirstName(), name.getMiddleName().orElse(DEFAULT_MIDDLE_NAME), name.getLastName());
+      })
+      .orElseThrow(() -> newIllegalArgumentException("Nameable must not be null"));
+  }
+
+  /**
+   * Factory method used to parse the given {@link String} into parts of a name and construct
+   * a new instance of {@link Name}.
+   *
+   * @param name {@link String} containing the name to parse.
+   * @return a new instance of {@link Name} initialized with the given {@link String name}
+   * parsed into individual name parts.
+   * @throws IllegalArgumentException if either the first or last name are not specified.
+   * @see #of(String, String, String)
+   * @see #of(String, String)
+   * @see #parseName(String)
+   */
+  public static Name of(String name) {
+    String[] parts = parseName(name);
+    return (parts.length < 3 ? of(parts[0], parts[1]) : of(parts[0], parts[1], parts[2]));
+  }
+
+  /**
+   * Factory method used to construct a new instance of {@link Name} initialized to the given {@link String first}
+   * and {@link String last} names.
+   *
+   * @param firstName {@link String} containing the first name.
+   * @param lastName {@link String} containing the last name.
+   * @throws IllegalArgumentException if first or last name are not specified.
+   * @return a new instance of {@link Name} initialized to the given {@link String first} and {@link String last} names.
+   * @throws IllegalArgumentException if either first or last name are not specified.
+   * @see #of(String, String, String)
+   */
+  public static Name of(String firstName, String lastName) {
+    return of(firstName, DEFAULT_MIDDLE_NAME, lastName);
+  }
+
+  /**
+   * Factory method used to construct a new instance of {@link Name} initialized to the given {@link String first},
+   * {@link String middle} and {@link String last} names.
+   *
+   * @param firstName {@link String} containing the first name.
+   * @param middleName {@link String} containing the middle name; May just be the initial(s) or even {@literal null}.
+   * @param lastName {@link String} containing the last name.
+   * @return a new instance of {@link Name} initialized to the given {@link String first}, {@link String middle}
+   * and {@link String last} names.
+   * @throws IllegalArgumentException if first or last name are not specified.
+   * @see #Name(String, String, String)
+   */
+  public static Name of(String firstName, String middleName, String lastName) {
+    return new Name(firstName, middleName, lastName);
+  }
+
+  /**
+   * Parses the given {@link String} representing a name.
+   *
+   * @param name {@link String} containing the name to parse.
+   * @return an array of {@link String Strings} containing the parts of the name.
+   * @throws IllegalArgumentException if {@code name} is {@literal null}
+   * or does not minimally consist of both a first and last name.
+   */
+  protected static String[] parseName(String name) {
+    return Optional.ofNullable(name)
+      .filter(StringUtils::hasText)
+      .map(StringUtils::trim)
+      .map(localName -> localName.split("\\s+"))
+      .filter(nameParts -> nameParts.length >= 2)
+      .orElseThrow(() -> newIllegalArgumentException("First and last name are required; was [%s]", name));
+  }
+
+  /**
+   * Constructs a new instance of {@link Name} initialized with the given first name, middle name
+   * (or initial(s) or event {@literal null}) and last name.
+   *
+   * @param firstName {@link String} containing the first name.
+   * @param middleName {@link String} containing the middle name; May just be the initial(s) or even {@literal null}.
+   * @param lastName {@link String} containing the last name.
+   * @throws IllegalArgumentException if first or last name are not specified.
+   */
+  private Name(String firstName, String middleName, String lastName) {
+    Assert.hasText(firstName, "First name is required");
+    Assert.hasText(lastName, "Last name is required");
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.middleName = Optional.ofNullable(middleName).filter(StringUtils::hasText).orElse(DEFAULT_MIDDLE_NAME);
+  }
+
+  /**
+   * Return the first name.
+   *
+   * @return a {@link String} containing the first name.
+   * @see java.lang.String
+   */
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  /**
+   * Return the last name.
+   *
+   * @return a {@link String} containing the last name.
+   * @see java.lang.String
+   */
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  /**
+   * Return the middle name. May just be the initial(s).
+   *
+   * @return an {@link Optional} containing the middle name.
+   * @see java.lang.String
+   * @see java.util.Optional
+   */
+  public Optional<String> getMiddleName() {
+    return Optional.ofNullable(this.middleName).filter(StringUtils::hasText);
+  }
+
+  /**
+   * Returns this {@link Name}.
+   *
+   * @return this {@link Name}.
+   * @see org.cp.elements.lang.Nameable#getName()
+   */
+  @Override
+  public Name getName() {
+    return this;
+  }
+
+  /**
+   * Builder method used to change the last name.
+   *
+   * This method returns a new instance of {@link Name} initialized with this {@link Name Name's}
+   * {@link #getFirstName() first name}, {@link #getMiddleName() middle name} and the given,
+   * new {@link String last name}.
+   *
+   * @param lastName {@link String} containing the new last name.
+   * @return a new instance of {@link Name} with the new {@link String last name}.
+   * @see #of(String, String, String)
+   * @see #getFirstName()
+   * @see #getMiddleName()
+   */
+  public Name change(String lastName) {
+    return of(this.getFirstName(), this.getMiddleName().orElse(DEFAULT_MIDDLE_NAME), lastName);
+  }
+
+  /**
+   * Compares this {@link Name} with the other given {@link Name} to determine the natural ordering (sort)
+   * of the {@link Name names}.
+   *
+   * The nature order of {@link Name names} as determined by this method is last name, first name
+   * and then middle name (or initial(s)) in ascending order.
+   *
+   * @param other {@link Name} being compared with this {@link Name}.
+   * @return a integer value indicating the natural order of this {@link Name} relative to the other {@link Name}.
+   * A &lt; 0 indicates this {@link Name} comes before the other {@link Name}, &gt; 0 indicates this {@link Name}
+   * comes after the other {@link Name} and 0 indicates both {@link Name names} are equal.
+   * @see org.cp.elements.util.ComparatorResultBuilder
+   * @see java.lang.Comparable#compareTo(Object)
+   */
+  @Override
+  public int compareTo(Name other) {
+    return ComparatorResultBuilder.<String>create()
+      .doCompare(this.getLastName(), other.getLastName())
+      .doCompare(this.getFirstName(), other.getFirstName())
+      .doCompare(this.getMiddleName().orElse("null"), other.getMiddleName().orElse("null"))
+      .build();
+  }
+
+  /**
+   * Determines whether this {@link Name} is equal to the given {@link Object}.
+   *
+   * @param obj {@link Object} being evaluated for equality with this {@link Name}.
+   * @return a boolean value indicating whether this {@link Name} is equal to the given {@link Object}.
+   * @see java.lang.Object#equals(Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+
+    if (!(obj instanceof Name)) {
+      return false;
+    }
+
+    Name that = (Name) obj;
+
+    return ObjectUtils.equals(this.getFirstName(), that.getFirstName())
+      && ObjectUtils.equals(this.getMiddleName(), that.getMiddleName())
+      && ObjectUtils.equals(this.getLastName(), that.getLastName());
+  }
+
+  /**
+   * Computes the hash code of this {@link Name}.
+   *
+   * @return the computed hash code of this {@link Name} as an {@link Integer}.
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    int hashValue = 17;
+    hashValue = 37 * hashValue + ObjectUtils.hashCode(this.getFirstName());
+    hashValue = 37 * hashValue + ObjectUtils.hashCode(this.getMiddleName());
+    hashValue = 37 * hashValue + ObjectUtils.hashCode(this.getLastName());
+    return hashValue;
+  }
+
+  /**
+   * Returns a {@link String} representation of this {@link Name}.
+   *
+   * @return a {@link String} containing the current state of this {@link Name}.
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    StringBuilder name = new StringBuilder(getFirstName());
+
+    getMiddleName().ifPresent(middleName -> name.append(NAME_PART_SEPARATOR).append(middleName));
+
+    name.append(NAME_PART_SEPARATOR);
+    name.append(getLastName());
+
+    return name.toString();
+  }
+}
---
+++ src/test/java/org/cp/domain/core/model/NameTests.java
@@ -0,0 +1,342 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.domain.core.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import org.cp.elements.io.IOUtils;
+import org.cp.elements.lang.Nameable;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ * Unit tests for {@link Name}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.junit.runner.RunWith
+ * @see org.mockito.Mock
+ * @see org.mockito.Mockito
+ * @see org.mockito.junit.MockitoJUnitRunner
+ * @see org.cp.domain.core.model.Name
+ * @since 1.0.0
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class NameTests {
+
+  @Mock
+  private Nameable<Name> mockNameable;
+
+  @Test
+  public void ofNameableWithName() {
+    Name name = Name.of("Jon", "Jason", "Bloom");
+
+    when(mockNameable.getName()).thenReturn(name);
+
+    Name copy = Name.of(mockNameable);
+
+    assertThat(copy).isNotNull();
+    assertThat(copy).isNotSameAs(name);
+    assertThat(copy.getFirstName()).isEqualTo(name.getFirstName());
+    assertThat(copy.getMiddleName()).isEqualTo(name.getMiddleName());
+    assertThat(copy.getLastName()).isEqualTo(name.getLastName());
+
+    verify(mockNameable, times(1)).getName();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void withNullNameableThrowsIllegalArgumentException() {
+    try {
+      Name.of((Nameable<Name>) null);
+    }
+    catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("Nameable must not be null");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  public void ofStringContainingFirstAndLastName() {
+    Name name = Name.of("Jon Bloom");
+
+    assertThat(name).isNotNull();
+    assertThat(name.getName()).isSameAs(name);
+    assertThat(name.getFirstName()).isEqualTo("Jon");
+    assertThat(name.getMiddleName().isPresent()).isFalse();
+    assertThat(name.getLastName()).isEqualTo("Bloom");
+  }
+
+  @Test
+  public void ofStringContainingFirstMiddleAndLastName() {
+    Name name = Name.of("Jon Jason Bloom");
+
+    assertThat(name).isNotNull();
+    assertThat(name.getName()).isSameAs(name);
+    assertThat(name.getFirstName()).isEqualTo("Jon");
+    assertThat(name.getMiddleName().orElse(null)).isEqualTo("Jason");
+    assertThat(name.getLastName()).isEqualTo("Bloom");
+  }
+
+  @Test
+  public void ofStringContainingFirstMiddleLastAndSuffixName() {
+    Name name = Name.of("Jon Jason Bloom Jr.");
+
+    assertThat(name).isNotNull();
+    assertThat(name.getName()).isSameAs(name);
+    assertThat(name.getFirstName()).isEqualTo("Jon");
+    assertThat(name.getMiddleName().orElse(null)).isEqualTo("Jason");
+    assertThat(name.getLastName()).isEqualTo("Bloom");
+  }
+
+  @Test
+  public void ofStringContainingPaddedFirstMiddleAndLastName() {
+    Name name = Name.of("  Jon   J Bloom ");
+
+    assertThat(name).isNotNull();
+    assertThat(name.getName()).isSameAs(name);
+    assertThat(name.getFirstName()).isEqualTo("Jon");
+    assertThat(name.getMiddleName().orElse(null)).isEqualTo("J");
+    assertThat(name.getLastName()).isEqualTo("Bloom");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void ofStringContainingOnlyFirstNameThrowsIllegalArgumentException() {
+    try {
+      Name.of("Jon");
+    }
+    catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("First and last name are required; was [Jon]");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void ofEmptyStringThrowsIllegalArgumentException() {
+    try {
+      Name.of("  ");
+    }
+    catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("First and last name are required; was [  ]");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void ofNullStringThrowsIllegalArgumentException() {
+    try {
+      Name.of((String) null);
+    }
+    catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("First and last name are required; was [null]");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  public void ofFirstAndLastName() {
+    Name name = Name.of("Jon", "Bloom");
+
+    assertThat(name).isNotNull();
+    assertThat(name.getName()).isSameAs(name);
+    assertThat(name.getFirstName()).isEqualTo("Jon");
+    assertThat(name.getMiddleName().isPresent()).isFalse();
+    assertThat(name.getLastName()).isEqualTo("Bloom");
+  }
+
+  @Test
+  public void ofFirstMiddleAndLastName() {
+    Name name = Name.of("Jon", "Jason", "Bloom");
+
+    assertThat(name).isNotNull();
+    assertThat(name.getName()).isSameAs(name);
+    assertThat(name.getFirstName()).isEqualTo("Jon");
+    assertThat(name.getMiddleName().orElse(null)).isEqualTo("Jason");
+    assertThat(name.getLastName()).isEqualTo("Bloom");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructWithNoFirstName() {
+    try {
+      Name.of(" ", "J", "Bloom");
+    }
+    catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("First name is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructWithNoLastName() {
+    try {
+      Name.of("Jon", " ", "");
+    }
+    catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("Last name is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  public void changeLastName() {
+    Name name = Name.of("Jon", "Bloom");
+
+    assertThat(name).isNotNull();
+    assertThat(name.getName()).isSameAs(name);
+    assertThat(name.getFirstName()).isEqualTo("Jon");
+    assertThat(name.getLastName()).isEqualTo("Bloom");
+    assertThat(name.getMiddleName().orElse(null)).isNull();
+
+    Name nameChange = name.change("Doe");
+
+    assertThat(nameChange).isNotNull();
+    assertThat(nameChange).isNotSameAs(name);
+    assertThat(nameChange.getFirstName()).isEqualTo("Jon");
+    assertThat(nameChange.getLastName()).isEqualTo("Doe");
+    assertThat(nameChange.getMiddleName().orElse(null)).isNull();
+  }
+
+  @Test
+  public void compareToIsLessThan() {
+    Name ellieBloom = Name.of("Ellie", "Bloom");
+    Name jonBloom = Name.of("Jon", "Bloom");
+
+    assertThat(ellieBloom).isNotNull();
+    assertThat(jonBloom).isNotNull();
+    assertThat(ellieBloom).isNotSameAs(jonBloom);
+    assertThat(ellieBloom.compareTo(jonBloom)).isLessThan(0);
+  }
+
+  @Test
+  public void compareToIsGreaterThan() {
+    Name sarahBloom = Name.of("Sarah", "Bloom");
+    Name jonBloom = Name.of("Jon", "Bloom");
+
+    assertThat(sarahBloom).isNotNull();
+    assertThat(jonBloom).isNotNull();
+    assertThat(sarahBloom).isNotSameAs(jonBloom);
+    assertThat(sarahBloom.compareTo(jonBloom)).isGreaterThan(0);
+  }
+
+  @Test
+  public void compareToIsEqual() {
+    Name jonBloomOne = Name.of("Jon", "Bloom");
+    Name jonBloomTwo = Name.of("Jon", "Bloom");
+
+    assertThat(jonBloomOne).isNotNull();
+    assertThat(jonBloomTwo).isNotNull();
+    assertThat(jonBloomOne).isNotSameAs(jonBloomTwo);
+    assertThat(jonBloomOne.compareTo(jonBloomTwo)).isEqualTo(0);
+  }
+
+  @Test
+  public void equalsWithEqualNamesIsTrue() {
+    Name jonBloomOne = Name.of("Jon", "J", "Bloom");
+    Name jonBloomTwo = Name.of("Jon", "J", "Bloom");
+
+    assertThat(jonBloomOne).isNotNull();
+    assertThat(jonBloomTwo).isNotNull();
+    assertThat(jonBloomOne).isNotSameAs(jonBloomTwo);
+    assertThat(jonBloomOne.equals(jonBloomTwo)).isTrue();
+  }
+
+  @Test
+  public void equalsWithAlmostEqualNamesIsFalse() {
+    Name jonBloomOne = Name.of("Jon", "J", "Bloom");
+    Name jonBloomTwo = Name.of("Jon", "Bloom");
+
+    assertThat(jonBloomOne).isNotNull();
+    assertThat(jonBloomTwo).isNotNull();
+    assertThat(jonBloomOne).isNotSameAs(jonBloomTwo);
+    assertThat(jonBloomOne.equals(jonBloomTwo)).isFalse();
+  }
+
+  @Test
+  public void equalsWithSimilarNamesIsFalse() {
+    Name johnBlum = Name.of("John", "J", "Blum");
+    Name jonBloom = Name.of("Jon", "J", "Bloom");
+
+    assertThat(johnBlum).isNotNull();
+    assertThat(jonBloom).isNotNull();
+    assertThat(johnBlum).isNotSameAs(jonBloom);
+    assertThat(johnBlum.equals(jonBloom)).isFalse();
+  }
+
+  @Test
+  public void hashCodeForNameIsNotZero() {
+    assertThat(Name.of("Jon", "J", "Bloom").hashCode()).isNotEqualTo(0);
+  }
+
+  @Test
+  public void hashCodeForIdenticalNamesAreEqual() {
+    assertThat(Name.of("Jon", "Bloom").hashCode()).isEqualTo(Name.of("Jon", "Bloom").hashCode());
+  }
+
+  @Test
+  public void hashCodeForSimilarNamesAreNotEqual() {
+    assertThat(Name.of("Jon", "J", "Bloom").hashCode()).isNotEqualTo(Name.of("Jon", "Bloom").hashCode());
+  }
+
+  @Test
+  public void toStringWithFirstAndLastNameIsCorrect() {
+    assertThat(Name.of("Jon", "Bloom").toString()).isEqualTo("Jon Bloom");
+  }
+
+  @Test
+  public void toStringWithFirstMiddleAndLastNameIsCorrect() {
+    assertThat(Name.of("Jon", "J", "Bloom").toString()).isEqualTo("Jon J Bloom");
+    assertThat(Name.of("Jon", "Jason", "Bloom").toString()).isEqualTo("Jon Jason Bloom");
+  }
+
+  @Test
+  public void nameIsSerializable() throws IOException, ClassNotFoundException {
+    Name name = Name.of("Jon", "J", "Bloom");
+
+    assertThat(name).isNotNull();
+
+    byte[] nameBytes = IOUtils.serialize(name);
+
+    assertThat(nameBytes).isNotNull();
+    assertThat(nameBytes).isNotEmpty();
+
+    Name deserializedName = IOUtils.deserialize(nameBytes);
+
+    assertThat(deserializedName).isNotNull();
+    assertThat(deserializedName).isNotSameAs(name);
+    assertThat(deserializedName.getFirstName()).isEqualTo(name.getFirstName());
+    assertThat(deserializedName.getMiddleName()).isEqualTo(name.getMiddleName());
+    assertThat(deserializedName.getLastName()).isEqualTo(name.getLastName());
+  }
+}
