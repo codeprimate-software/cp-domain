7e76609eb0dd84f50606f04234e0d10ddeddcdf5
John Blum <jblum@vmware.com>
2023-02-16 13:51:15 -0800

    Review and refactor the org.cp.domain.geo.model.usa.ZIP class.

--- src/main/java/org/cp/domain/geo/model/usa/ZIP.java
+++ src/main/java/org/cp/domain/geo/model/usa/ZIP.java
@@ -13,100 +13,207 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.domain.geo.model.usa;
 
 import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalArgumentException;
 
 import java.util.Optional;
+import java.util.function.Predicate;
 
+import org.cp.domain.geo.enums.Country;
+import org.cp.domain.geo.enums.State;
 import org.cp.domain.geo.model.PostalCode;
+import org.cp.domain.geo.model.usa.support.StateZipCodesRepository;
+import org.cp.elements.lang.Assert;
 import org.cp.elements.lang.StringUtils;
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.NullSafe;
+import org.cp.elements.lang.annotation.Nullable;
 
 /**
- * The {@link ZIP} class models the Zone Improvement Plan (ZIP) system of postal codes used by the United States
- * Postal Service since 1963 to assist in the sorting of mail.
+ * Abstract Data Type (ADT) and {@link PostalCode} implementation modeling the {@literal Zone Improvement Plan (ZIP)}
+ * system of {@literal postal codes} used by the {@link Country#UNITED_STATES_OF_AMERICA United States}
+ * Postal Service since 1963 to assist in the sorting and routing of mail.
  *
  * @author John Blum
- * @see java.io.Serializable
- * @see java.lang.Comparable
+ * @see org.cp.domain.geo.enums.Country
+ * @see org.cp.domain.geo.enums.State
  * @see org.cp.domain.geo.model.PostalCode
- * @since 1.0.0
+ * @see org.cp.domain.geo.model.usa.support.StateZipCodesRepository
+ * @see <a href="https://www.50states.com/zipcodes/">All About ZIP Codes</a>
+ * @since 0.1.0
  */
 @SuppressWarnings("unused")
 public class ZIP extends PostalCode {
 
-  private final String code;
+  protected static final int STANDARD_POSTAL_CODE_LENGTH = 5;
+  protected static final int EXTENSION_POSTAL_CODE_LENGTH = 9;
 
-  private String plusFourExtension;
+  protected static final String ZIP_TO_STRING = "%1$s%2$s";
+  protected static final String ZIP_CODE_EXTENSION = "-%s";
 
-  public static ZIP of(String code) {
-    return new ZIP(code);
-  }
+  /**
+   * Factory method used to construct a new instance of {@link ZIP} copied from the exiting,
+   * required {@link PostalCode}.
+   *
+   * @param postalCode {@link PostalCode} to copy; must not be {@literal null}.
+   * @return a new {@link ZIP} copied from the exiting {@link PostalCode}.
+   * @throws IllegalArgumentException if the given {@link PostalCode} is {@literal null}.
+   * @see org.cp.domain.geo.model.PostalCode
+   * @see #of(String)
+   */
+  public static @NotNull ZIP from(@NotNull PostalCode postalCode) {
 
-  public ZIP(String code) {
-    super(code);
+    Assert.notNull(postalCode, "PostalCode to copy is required");
 
-    this.code = Optional.ofNullable(code)
-      .filter(StringUtils::hasText)
-      .map(StringUtils::getDigits)
-      .map(StringUtils::length)
-      .filter(length -> length == 5 || length == 9)
-      .map(length -> code)
-      .orElseThrow(() -> newIllegalArgumentException("5 or 9 digit postal code number [%s] is required", code));
+    return postalCode instanceof ZIP ? (ZIP) postalCode
+      : of(postalCode.getNumber());
   }
 
   /**
-   * Returns a 5 or 9 digit number of the postal code.
+   * Factory method used to construct a new instance of {@link ZIP} initialized with the given,
+   * required {@link String postal code}.
    *
-   * @return a {@link String} containing the 5 or 9 digit number of the postal code.
+   * @param code {@link String} containing the {@literal postal code} for the {@link ZIP};
+   * must not be {@literal null} or {@literal empty}; must be either {@literal 5 digits} of {@literal 9 digits}.
+   * @return a new {@link ZIP} initialized with the given {@link String postal code}.
+   * @throws IllegalArgumentException if the given {@link String postal code} is not valid.
    */
-  public String getCode() {
+  public static @NotNull ZIP of(@NotNull String code) {
+    return new ZIP(code);
+  }
+
+  private final String code;
+
+  private String fourDigitExtension;
+
+  /**
+   * Constructs a new instance of {@link ZIP} initialized with the given, required {@link String postal code}.
+   *
+   * @param code {@link String} containing the {@literal postal code} defining this {@link ZIP};
+   * must not be {@literal null} or {@literal empty}.
+   * @throws IllegalArgumentException if the given {@link String postal code} is {@literal null} or {@literal empty},
+   * or the {@link String postal code} is not the standard format of {@literal 5} or {@literal 9} digits.
+   */
+  public ZIP(@NotNull String code) {
+
+    super(code);
+
+    String postalCode = Optional.ofNullable(code)
+      .filter(postalCodePredicate())
+      .map(StringUtils::getDigits)
+      .orElseThrow(() -> newIllegalArgumentException("5 or 9 digit postal code is required; but was [%s]", code));
+
+    this.code = postalCode.substring(0, STANDARD_POSTAL_CODE_LENGTH);
+    this.fourDigitExtension = postalCode.substring(STANDARD_POSTAL_CODE_LENGTH);
+  }
+
+  @NullSafe
+  private @NotNull Predicate<String> postalCodePredicate() {
+
+    return postalCode -> {
+      int length = StringUtils.length(StringUtils.getDigits(postalCode));
+      return length == STANDARD_POSTAL_CODE_LENGTH || length == EXTENSION_POSTAL_CODE_LENGTH;
+    };
+  }
+
+  /**
+   * Returns the {@link String 4 digit postal code}, or possibly the {@link String 9 digit postal code},
+   * of this {@link ZIP}.
+   *
+   * @return the {@link String 4 digit postal code}, or possibly the {@link String 9 digit postal code},
+   * of this {@link ZIP}.
+   */
+  public @NotNull String getCode() {
     return this.code;
   }
 
   /**
-   * Returns the {@link String number} of the {@link PostalCode}.
+   * Returns the {@link Country#UNITED_STATES_OF_AMERICA}.
    *
-   * @return the {@link String number} of the {@link PostalCode}.
-   * @see java.lang.String
-   */
-  @Override
-  public String getNumber() {
-    return StringUtils.getDigits(getCode() + getPlusFourExtension().orElse(StringUtils.EMPTY_STRING));
-  }
-
-  /**
-   * Returns the {@link Optional} 4 digit number extension of the postal code.
-   *
-   * @return an {@link Optional} {@link String} containing the 4 digit number extension of the postal code.
-   * @see java.lang.String
+   * @return the {@link Country#UNITED_STATES_OF_AMERICA}.
+   * @see org.cp.domain.geo.enums.Country#UNITED_STATES_OF_AMERICA
    * @see java.util.Optional
    */
-  public Optional<String> getPlusFourExtension() {
-    return Optional.ofNullable(plusFourExtension).filter(StringUtils::hasText);
+  @Override
+  public final Optional<Country> getCountry() {
+    return Optional.of(Country.UNITED_STATES_OF_AMERICA);
   }
 
   /**
-   * Builder method to set the {@link ZIP} code four digit number extension.
+   * Returns the {@link Optional} {@link String 4 digit postal extension} of this {@link ZIP}.
    *
-   * @param plusFourExtension {@link String} containing the {@link ZIP} code four digit number extension.
+   * @return the {@link Optional} {@link String 4 digit postal extension} of this {@link ZIP}.
+   * @see java.util.Optional
+   * @see java.lang.String
+   */
+  public Optional<String> getFourDigitExtension() {
+
+    return Optional.ofNullable(this.fourDigitExtension)
+      .filter(StringUtils::hasText);
+  }
+
+  /**
+   * Returns the complete, {@link String 9 digit postal code} of this {@link ZIP}.
+   *
+   * @return the complete, {@link String 9 digit postal code} of this {@link ZIP}.
+   * @see #codePlusFourDigitExtension()
+   * @see java.lang.String
+   * @see #getCode()
+   */
+  @Override
+  public @NotNull String getNumber() {
+    String number = codePlusFourDigitExtension();
+    return StringUtils.truncate(StringUtils.getDigits(number), EXTENSION_POSTAL_CODE_LENGTH);
+  }
+
+  /**
+   * Gets the {@link State} in which this {@link ZIP} is assigned.
+   *
+   * @return the {@link State} in which this {@link ZIP} is assigned.
+   * @throws IllegalArgumentException if this {@link ZIP} is not assigned to a {@link State}.
+   * @see org.cp.domain.geo.enums.State
+   * @see #getCode()
+   */
+  public @NotNull State getState() {
+    return StateZipCodesRepository.getInstance().findBy(this);
+  }
+
+  private @NotNull String codePlusFourDigitExtension() {
+    return getCode() + resolveFourDigitExtension();
+  }
+
+  /**
+   * Builder method used to set the {@link ZIP} code {@link String four-digit postal extension}.
+   *
+   * @param fourDigitExtension {@link String} containing the {@link ZIP} code
+   * {@literal four-digit postal extension}.
    * @return this {@link ZIP}.
    */
-  public ZIP plusFour(String plusFourExtension) {
-    this.plusFourExtension = plusFourExtension;
+  public @NotNull ZIP plusFour(@Nullable String fourDigitExtension) {
+    this.fourDigitExtension = fourDigitExtension;
     return this;
   }
 
+  private @NotNull String resolveFourDigitExtension() {
+    return getFourDigitExtension().orElse(StringUtils.EMPTY_STRING);
+  }
+
   /**
-   * Returns a {@link String} representation of this {@link ZIP}.
+   * Returns a {@link String} representation of this {@link ZIP} code.
    *
    * @return a {@link String} describing this {@link ZIP} code.
    * @see java.lang.Object#toString()
+   * @see #getFourDigitExtension()
+   * @see #getCode()
    */
   @Override
-  public String toString() {
-    return String.format("%1$s%2$s", getCode(),
-      getPlusFourExtension().map(code -> String.format("-%s", code)).orElse(StringUtils.EMPTY_STRING));
+  public @NotNull String toString() {
+
+    String fourDigitExtension = getFourDigitExtension()
+      .map(extension -> String.format(ZIP_CODE_EXTENSION, extension))
+      .orElse(StringUtils.EMPTY_STRING);
+
+    return String.format(ZIP_TO_STRING, getCode(), fourDigitExtension);
   }
 }
---
+++ src/test/java/org/cp/domain/geo/model/usa/ZipUnitTests.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.domain.geo.model.usa;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.withSettings;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.junit.Test;
+
+import org.cp.domain.geo.enums.Country;
+import org.cp.domain.geo.enums.State;
+import org.cp.domain.geo.model.PostalCode;
+
+import org.mockito.quality.Strictness;
+
+/**
+ * Unit Tests for {@link ZIP}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.domain.geo.model.PostalCode
+ * @see org.cp.domain.geo.model.usa.ZIP
+ * @since 0.1.0
+ */
+public class ZipUnitTests {
+
+  @Test
+  public void constructNewZipWithFiveDigitPostalCode() {
+
+    ZIP zip = new ZIP("12345");
+
+    assertThat(zip).isNotNull();
+    assertThat(zip.getCode()).isEqualTo("12345");
+    assertThat(zip.getFourDigitExtension()).isNotPresent();
+    assertThat(zip.getNumber()).isEqualTo("12345");
+  }
+
+  @Test
+  public void constructNewZipWithNineDigitPostalCode() {
+
+    ZIP zip = new ZIP("12345-6789");
+
+    assertThat(zip).isNotNull();
+    assertThat(zip.getCode()).isEqualTo("12345");
+    assertThat(zip.getFourDigitExtension().orElse(null)).isEqualTo("6789");
+    assertThat(zip.getNumber()).isEqualTo("123456789");
+  }
+
+  @Test
+  public void constructNewZipWithIllegalPostalCode() {
+
+    Arrays.asList("  ", "", null).forEach(postalCode ->
+      assertThatIllegalArgumentException()
+        .isThrownBy(() -> new ZIP(postalCode))
+        .withMessage("Number [%s] is required", postalCode)
+        .withNoCause());
+  }
+
+  @Test
+  public void constructPostalCodeWithInvalidPostalCode() {
+
+    Arrays.asList("1234", "123456", "1234567", "1234567890").forEach(postalCode ->
+      assertThatIllegalArgumentException()
+        .isThrownBy(() -> new ZIP(postalCode))
+        .withMessage("5 or 9 digit postal code is required; but was [%s]", postalCode)
+        .withNoCause());
+  }
+
+  @Test
+  public void fromZip() {
+
+    ZIP zip = new ZIP("12345");
+    ZIP copy = ZIP.from(zip);
+
+    assertThat(copy).isSameAs(zip);
+  }
+
+  @Test
+  public void fromPostalCode() {
+
+    PostalCode mockPostalCode = mock(PostalCode.class, withSettings().strictness(Strictness.LENIENT));
+
+    doReturn("12345").when(mockPostalCode).getNumber();
+    doReturn(Optional.of(Country.GERMANY)).when(mockPostalCode).getCountry();
+
+    ZIP zip = ZIP.from(mockPostalCode);
+
+    assertThat(zip).isNotNull();
+    assertThat(zip.getCode()).isEqualTo("12345");
+    assertThat(zip.getFourDigitExtension()).isNotPresent();
+    assertThat(zip.getNumber()).isEqualTo("12345");
+    assertThat(zip.getCountry().orElse(null)).isEqualTo(Country.UNITED_STATES_OF_AMERICA);
+
+    verify(mockPostalCode, times(1)).getNumber();
+    verifyNoMoreInteractions(mockPostalCode);
+  }
+
+  @Test
+  public void fromNonCompliantPostalCode() {
+
+    PostalCode mockPostalCode = mock(PostalCode.class);
+
+    doReturn("123").when(mockPostalCode).getNumber();
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> ZIP.from(mockPostalCode))
+      .withMessage("5 or 9 digit postal code is required; but was [123]")
+      .withNoCause();
+
+    verify(mockPostalCode, times(1)).getNumber();
+    verifyNoMoreInteractions(mockPostalCode);
+  }
+
+  @Test
+  public void fromNull() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> ZIP.from(null))
+      .withMessage("PostalCode to copy is required")
+      .withNoCause();
+  }
+
+  @Test
+  public void ofCodeHavingFiveDigitPostalCode() {
+
+    ZIP zip = ZIP.of("12345");
+
+    assertThat(zip).isNotNull();
+    assertThat(zip.getCode()).isEqualTo("12345");
+    assertThat(zip.getFourDigitExtension()).isNotPresent();
+    assertThat(zip.getNumber()).isEqualTo("12345");
+  }
+
+  @Test
+  public void ofCodeHavingNineDigitPostalCode() {
+
+    ZIP zip = ZIP.of("12345-6789");
+
+    assertThat(zip).isNotNull();
+    assertThat(zip.getCode()).isEqualTo("12345");
+    assertThat(zip.getFourDigitExtension().orElse(null)).isEqualTo("6789");
+    assertThat(zip.getNumber()).isEqualTo("123456789");
+  }
+
+  @Test
+  public void getCountry() {
+    assertThat(ZIP.of("12345").getCountry().orElse(null)).isEqualTo(Country.UNITED_STATES_OF_AMERICA);
+  }
+
+  @Test
+  public void getStateIsValid() {
+    assertThat(ZIP.of("97205").getState()).isEqualTo(State.OREGON);
+  }
+
+  @Test
+  public void getStateIsInvalid() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> ZIP.of("00001").getState())
+      .withMessage("State for ZIP code [00001] not found")
+      .withNoCause();
+  }
+
+  @Test
+  public void plusFourIsCorrect() {
+
+    ZIP zip = ZIP.of("12345");
+
+    assertThat(zip).isNotNull();
+    assertThat(zip.getCode()).isEqualTo("12345");
+    assertThat(zip.getFourDigitExtension()).isNotPresent();
+    assertThat(zip.getNumber()).isEqualTo("12345");
+    assertThat(zip.plusFour("6789")).isSameAs(zip);
+    assertThat(zip.getCode()).isEqualTo("12345");
+    assertThat(zip.getFourDigitExtension().orElse(null)).isEqualTo("6789");
+    assertThat(zip.getNumber()).isEqualTo("123456789");
+    assertThat(zip.plusFour(null)).isSameAs(zip);
+    assertThat(zip.getCode()).isEqualTo("12345");
+    assertThat(zip.getFourDigitExtension()).isNotPresent();
+    assertThat(zip.getNumber()).isEqualTo("12345");
+  }
+
+  @Test
+  public void toStringWithFiveDigitPostalCodeIsCorrect() {
+    assertThat(ZIP.of("12345").toString()).isEqualTo("12345");
+  }
+
+  @Test
+  public void toStringWithNineDigitPostalCodeIsCorrect() {
+    assertThat(ZIP.of("12345").plusFour("6789").toString()).isEqualTo("12345-6789");
+  }
+}
