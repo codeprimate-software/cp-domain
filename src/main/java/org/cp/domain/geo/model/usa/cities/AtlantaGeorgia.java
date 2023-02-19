df192b71aa25d5435c6657c1cf54f862a57f9ced
John Blum <jblum@vmware.com>
2023-02-18 22:48:44 -0800

    Review and refactor the org.cp.domain.geo.model.usa.cities.AtlantaGeorgia class.

--- src/main/java/org/cp/domain/geo/model/usa/cities/AtlantaGeorgia.java
+++ src/main/java/org/cp/domain/geo/model/usa/cities/AtlantaGeorgia.java
@@ -13,22 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.domain.geo.model.usa.cities;
 
-import static org.cp.elements.lang.RuntimeExceptionsFactory.newUnsupportedOperationException;
-
+import org.cp.domain.geo.enums.Country;
 import org.cp.domain.geo.enums.State;
-import org.cp.domain.geo.model.usa.UnitedStatesCity;
+import org.cp.domain.geo.model.usa.ImmutableUnitedStatesCity;
+import org.cp.elements.lang.annotation.Immutable;
 
 /**
- * The AtlantaGeorgia class...
+ * {@link ImmutableUnitedStatesCity} modeling the city of {@literal Atlanta} {@link State#GEORGIA},
+ * {@link Country#UNITED_STATES_OF_AMERICA USA}.
  *
  * @author John Blum
- * @since 1.0.0
+ * @see org.cp.domain.geo.enums.Country
+ * @see org.cp.domain.geo.enums.State
+ * @see org.cp.domain.geo.model.usa.ImmutableUnitedStatesCity
+ * @see org.cp.elements.lang.annotation.Immutable
+ * @since 0.1.0
  */
-@SuppressWarnings("unused")
-public final class AtlantaGeorgia extends UnitedStatesCity {
+@Immutable
+public final class AtlantaGeorgia extends ImmutableUnitedStatesCity {
 
   public static final AtlantaGeorgia INSTANCE = new AtlantaGeorgia();
 
@@ -37,17 +41,12 @@ public final class AtlantaGeorgia extends UnitedStatesCity {
   }
 
   @Override
-  public final boolean isCapital() {
+  public boolean isCapital() {
     return true;
   }
 
   @Override
-  public final State getState() {
+  public State getState() {
     return State.GEORGIA;
-  }
-
-  @Override
-  public final void setState(State state) {
-    throw newUnsupportedOperationException("State cannot be changed");
   }
 }
