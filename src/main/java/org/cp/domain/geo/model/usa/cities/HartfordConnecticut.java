3a9cdb88da4848f721baaad5c3a74fac40d5970f
John Blum <jblum@vmware.com>
2023-02-18 23:04:15 -0800

    Review and refactor the org.cp.domain.geo.model.usa.cities.HartfordConnecticut class.

--- src/main/java/org/cp/domain/geo/model/usa/cities/HartfordConnecticut.java
+++ src/main/java/org/cp/domain/geo/model/usa/cities/HartfordConnecticut.java
@@ -13,22 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.domain.geo.model.usa.cities;
 
-import static org.cp.elements.lang.RuntimeExceptionsFactory.newUnsupportedOperationException;
-
+import org.cp.domain.geo.enums.Country;
 import org.cp.domain.geo.enums.State;
-import org.cp.domain.geo.model.usa.UnitedStatesCity;
+import org.cp.domain.geo.model.usa.ImmutableUnitedStatesCity;
+import org.cp.elements.lang.annotation.Immutable;
 
 /**
- * The HartfordConnecticut class...
+ * {@link ImmutableUnitedStatesCity} modeling the city of {@literal HARTFORD} {@link State#CONNECTICUT},
+ * {@link Country#UNITED_STATES_OF_AMERICA USA}.
  *
  * @author John Blum
- * @since 1.0.0
+ * @see org.cp.domain.geo.enums.Country
+ * @see org.cp.domain.geo.enums.State
+ * @see org.cp.domain.geo.model.usa.ImmutableUnitedStatesCity
+ * @see org.cp.elements.lang.annotation.Immutable
+ * @since 0.1.0
  */
-@SuppressWarnings("unused")
-public final class HartfordConnecticut extends UnitedStatesCity {
+@Immutable
+public final class HartfordConnecticut extends ImmutableUnitedStatesCity {
 
   public static final HartfordConnecticut INSTANCE = new HartfordConnecticut();
 
@@ -37,17 +41,12 @@ public final class HartfordConnecticut extends UnitedStatesCity {
   }
 
   @Override
-  public final boolean isCapital() {
+  public boolean isCapital() {
     return true;
   }
 
   @Override
-  public final State getState() {
+  public State getState() {
     return State.CONNECTICUT;
-  }
-
-  @Override
-  public final void setState(State state) {
-    throw newUnsupportedOperationException("State cannot be changed");
   }
 }
