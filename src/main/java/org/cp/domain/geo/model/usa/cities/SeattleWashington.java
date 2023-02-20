352c8b6cc4de758f0231a7c14e5156c7b8efe93a
John Blum <jblum@vmware.com>
2023-02-19 16:52:44 -0800

    Review and refactor the org.cp.domain.geo.model.usa.cities.SeattleWashington class.

--- src/main/java/org/cp/domain/geo/model/usa/cities/SeattleWashington.java
+++ src/main/java/org/cp/domain/geo/model/usa/cities/SeattleWashington.java
@@ -13,22 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.domain.geo.model.usa.cities;
 
-import static org.cp.elements.lang.RuntimeExceptionsFactory.newUnsupportedOperationException;
-
+import org.cp.domain.geo.enums.Country;
 import org.cp.domain.geo.enums.State;
-import org.cp.domain.geo.model.usa.UnitedStatesCity;
+import org.cp.domain.geo.model.usa.ImmutableUnitedStatesCity;
+import org.cp.elements.lang.annotation.Immutable;
 
 /**
- * The SeattleWashington class...
+ * {@link ImmutableUnitedStatesCity} modeling the city of {@literal Seattle} {@link State#WASHINGTON},
+ * {@link Country#UNITED_STATES_OF_AMERICA USA}.
  *
  * @author John Blum
- * @since 1.0.0
+ * @see org.cp.domain.geo.enums.Country
+ * @see org.cp.domain.geo.enums.State
+ * @see org.cp.domain.geo.model.usa.ImmutableUnitedStatesCity
+ * @see org.cp.elements.lang.annotation.Immutable
+ * @since 0.1.0
  */
-@SuppressWarnings("unused")
-public final class SeattleWashington extends UnitedStatesCity {
+@Immutable
+public final class SeattleWashington extends ImmutableUnitedStatesCity {
 
   public static final SeattleWashington INSTANCE = new SeattleWashington();
 
@@ -37,12 +41,7 @@ public final class SeattleWashington extends UnitedStatesCity {
   }
 
   @Override
-  public final State getState() {
+  public State getState() {
     return State.WASHINGTON;
-  }
-
-  @Override
-  public final void setState(State state) {
-    throw newUnsupportedOperationException("State cannot be changed");
   }
 }
