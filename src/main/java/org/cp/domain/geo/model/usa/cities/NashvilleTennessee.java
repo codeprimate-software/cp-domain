1509ff7c742edf4ecdfd8bb5dbc402bc3f772a3d
John Blum <jblum@vmware.com>
2023-02-19 16:15:41 -0800

    Review and refactor the org.cp.domain.geo.model.usa.cities.NashvilleTennessee class.

--- src/main/java/org/cp/domain/geo/model/usa/cities/NashvilleTennessee.java
+++ src/main/java/org/cp/domain/geo/model/usa/cities/NashvilleTennessee.java
@@ -13,22 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.domain.geo.model.usa.cities;
 
-import static org.cp.elements.lang.RuntimeExceptionsFactory.newUnsupportedOperationException;
-
+import org.cp.domain.geo.enums.Country;
 import org.cp.domain.geo.enums.State;
-import org.cp.domain.geo.model.usa.UnitedStatesCity;
+import org.cp.domain.geo.model.usa.ImmutableUnitedStatesCity;
+import org.cp.elements.lang.annotation.Immutable;
 
 /**
- * The NashvilleTennessee class...
+ * {@link ImmutableUnitedStatesCity} modeling the city of {@literal Nashville} {@link State#TENNESSEE},
+ * {@link Country#UNITED_STATES_OF_AMERICA USA}.
  *
  * @author John Blum
- * @since 1.0.0
+ * @see org.cp.domain.geo.enums.Country
+ * @see org.cp.domain.geo.enums.State
+ * @see org.cp.domain.geo.model.usa.ImmutableUnitedStatesCity
+ * @see org.cp.elements.lang.annotation.Immutable
+ * @since 0.1.0
  */
-@SuppressWarnings("unused")
-public final class NashvilleTennessee extends UnitedStatesCity {
+@Immutable
+public final class NashvilleTennessee extends ImmutableUnitedStatesCity {
 
   public static final NashvilleTennessee INSTANCE = new NashvilleTennessee();
 
@@ -37,17 +41,12 @@ public final class NashvilleTennessee extends UnitedStatesCity {
   }
 
   @Override
-  public final boolean isCapital() {
+  public boolean isCapital() {
     return true;
   }
 
   @Override
-  public final State getState() {
+  public State getState() {
     return State.TENNESSEE;
-  }
-
-  @Override
-  public final void setState(State state) {
-    throw newUnsupportedOperationException("State cannot be changed");
   }
 }
