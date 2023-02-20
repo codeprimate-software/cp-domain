8545f98a9b0fea57afc4e20b9d892b52827e3ed0
John Blum <jblum@vmware.com>
2023-02-19 16:51:40 -0800

    Reivew and refactor the org.cp.domain.geo.model.usa.cities.SanFranciscoCalifornia class.

--- src/main/java/org/cp/domain/geo/model/usa/cities/SanFranciscoCalifornia.java
+++ src/main/java/org/cp/domain/geo/model/usa/cities/SanFranciscoCalifornia.java
@@ -13,22 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.domain.geo.model.usa.cities;
 
-import static org.cp.elements.lang.RuntimeExceptionsFactory.newUnsupportedOperationException;
-
+import org.cp.domain.geo.enums.Country;
 import org.cp.domain.geo.enums.State;
-import org.cp.domain.geo.model.usa.UnitedStatesCity;
+import org.cp.domain.geo.model.usa.ImmutableUnitedStatesCity;
+import org.cp.elements.lang.annotation.Immutable;
 
 /**
- * The SanFranciscoCalifornia class...
+ * {@link ImmutableUnitedStatesCity} modeling the city of {@literal San Francisco} {@link State#CALIFORNIA},
+ * {@link Country#UNITED_STATES_OF_AMERICA USA}.
  *
  * @author John Blum
- * @since 1.0.0
+ * @see org.cp.domain.geo.enums.Country
+ * @see org.cp.domain.geo.enums.State
+ * @see org.cp.domain.geo.model.usa.ImmutableUnitedStatesCity
+ * @see org.cp.elements.lang.annotation.Immutable
+ * @since 0.1.0
  */
-@SuppressWarnings("unused")
-public final class SanFranciscoCalifornia extends UnitedStatesCity {
+@Immutable
+public final class SanFranciscoCalifornia extends ImmutableUnitedStatesCity {
 
   public static final SanFranciscoCalifornia INSTANCE = new SanFranciscoCalifornia();
 
@@ -37,12 +41,7 @@ public final class SanFranciscoCalifornia extends UnitedStatesCity {
   }
 
   @Override
-  public final State getState() {
+  public State getState() {
     return State.CALIFORNIA;
-  }
-
-  @Override
-  public final void setState(State state) {
-    throw newUnsupportedOperationException("State cannot be changed");
   }
 }
