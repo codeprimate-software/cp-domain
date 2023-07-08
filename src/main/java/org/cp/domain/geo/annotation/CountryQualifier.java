bee65ff8db77a2262893a8af9272a8296b2c8bfd
John Blum <jblum@vmware.com>
2023-07-08 14:39:15 -0700

    Create CountryQualifer annotation in the org.cp.domain.geo.annotation package to qualify Address implementations by Country.
    The CountryQualifier annotation can be used by Java's ServiceLoader to locate (qualify) and load Address instances by type (Country).

---
+++ src/main/java/org/cp/domain/geo/annotation/CountryQualifier.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.domain.geo.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.cp.domain.geo.enums.Country;
+
+/**
+ * {@link Annotation Qualifier} by {@link Country}
+ *
+ * @author John Blum
+ * @see java.lang.annotation.Annotation
+ * @since 0.1.0
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.TYPE })
+public @interface CountryQualifier {
+
+  /**
+   * Configures the {@link Country} in which this annotated {@link Object} has been qualified and resides.
+   *
+   * @return the {@link Country} in which this annotated {@link Object} has been qualified and resides.
+   * @see org.cp.domain.geo.enums.Country
+   */
+  Country value() default Country.UNKNOWN;
+
+}
