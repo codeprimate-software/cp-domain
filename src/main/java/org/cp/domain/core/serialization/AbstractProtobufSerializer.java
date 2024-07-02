348c27b57fe2fbef28e6a4ade439add0f53ba530
John Blum <jblum0@gmail.com>
2024-07-01 23:55:41 -0700

    Add AbstractProtobufSerializer base class implementing the Elements Serializer interface for serialiing Protobuf messages to a byte array.

---
+++ src/main/java/org/cp/domain/core/serialization/AbstractProtobufSerializer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.domain.core.serialization;
+
+import java.nio.ByteBuffer;
+
+import com.google.protobuf.Message;
+
+import org.cp.elements.data.serialization.Serializer;
+import org.cp.elements.lang.Assert;
+
+/**
+ * Abstract base class and implementation of {@link Serializer} used to de/serialize Google Protobuf
+ * {@link Message Messages}.
+ *
+ * @author John Blum
+ * @see java.nio.ByteBuffer
+ * @see com.google.protobuf.Message
+ * @see org.cp.elements.data.serialization.Serializer
+ * @since 0.2.0
+ */
+public abstract class AbstractProtobufSerializer implements Serializer {
+
+  @Override
+  public ByteBuffer serialize(Object target) {
+
+    Assert.isInstanceOf(target, Message.class, "Object must be an instance of Message");
+
+    return ByteBuffer.wrap(((Message) target).toByteArray());
+  }
+}
