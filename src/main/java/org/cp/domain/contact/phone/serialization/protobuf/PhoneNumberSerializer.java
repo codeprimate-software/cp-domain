da695439c822dc67f6bf840bb0aa9243b7ad9a3b
John Blum <jblum0@gmail.com>
2024-07-13 19:48:44 -0700

    Create PhoneNumberSerializer to de/serialize a PhoneNumber using Protobuf.

---
+++ src/main/java/org/cp/domain/contact/phone/serialization/protobuf/PhoneNumberSerializer.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.domain.contact.phone.serialization.protobuf;
+
+import static org.cp.elements.lang.ElementsExceptionsFactory.newDeserializationException;
+
+import java.nio.ByteBuffer;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
+
+import org.cp.domain.contact.phone.model.PhoneNumber;
+import org.cp.domain.contact.phone.model.proto.PhoneNumberProto;
+import org.cp.domain.contact.phone.serialization.protobuf.converters.PhoneNumberConverter;
+import org.cp.domain.contact.phone.serialization.protobuf.converters.PhoneNumberProtoConverter;
+import org.cp.domain.core.serialization.protobuf.AbstractProtobufSerializer;
+import org.cp.elements.data.conversion.ConversionException;
+import org.cp.elements.data.serialization.Serializer;
+
+/**
+ * {@link Serializer} used to de/serialize a {@link PhoneNumber} using Protobuf
+ *
+ * @author John Blum
+ * @see org.cp.domain.contact.phone.model.PhoneNumber
+ * @see org.cp.domain.contact.phone.model.proto.PhoneNumberProto
+ * @see org.cp.domain.core.serialization.protobuf.AbstractProtobufSerializer
+ * @since 0.2.0
+ */
+public class PhoneNumberSerializer extends AbstractProtobufSerializer {
+
+  private final PhoneNumberConverter phoneNumberConverter = new PhoneNumberConverter();
+  private final PhoneNumberProtoConverter phoneNumberProtoConverter = new PhoneNumberProtoConverter();
+
+  protected PhoneNumberConverter getPhoneNumberConverter() {
+    return this.phoneNumberConverter;
+  }
+
+  protected PhoneNumberProtoConverter getPhoneNumberProtoConverter() {
+    return this.phoneNumberProtoConverter;
+  }
+
+  @Override
+  protected Message convert(Object target) {
+
+    if (target instanceof PhoneNumber phoneNumber) {
+      return getPhoneNumberConverter().convert(phoneNumber);
+    }
+
+    throw new ConversionException("Cannot convert [%s] into a Protobuf message".formatted(target));
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public <T> T deserialize(ByteBuffer bytes) {
+
+    try {
+      byte[] data = bytes.array();
+      return (T) getPhoneNumberProtoConverter().convert(PhoneNumberProto.PhoneNumber.parseFrom(data));
+    }
+    catch (InvalidProtocolBufferException cause) {
+      throw newDeserializationException(cause, "Failed to deserialize byte array as a PhoneNumber");
+    }
+  }
+}
---
+++ src/test/java/org/cp/domain/contact/contact/phone/serialization/protobuf/PhoneNumberSerializerIntegrationTests.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.domain.contact.contact.phone.serialization.protobuf;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.junit.jupiter.api.Test;
+
+import org.cp.domain.contact.phone.model.AreaCode;
+import org.cp.domain.contact.phone.model.ExchangeCode;
+import org.cp.domain.contact.phone.model.Extension;
+import org.cp.domain.contact.phone.model.LineNumber;
+import org.cp.domain.contact.phone.model.PhoneNumber;
+import org.cp.domain.contact.phone.serialization.protobuf.PhoneNumberSerializer;
+import org.cp.elements.io.IOUtils;
+
+/**
+ * Integration Tests for {@link PhoneNumberSerializer}.
+ *
+ * @author John Blum
+ * @see org.junit.jupiter.api.Test
+ * @see org.cp.domain.contact.phone.serialization.protobuf.PhoneNumberSerializer
+ * @since 0.2.0
+ */
+public class PhoneNumberSerializerIntegrationTests {
+
+  private final PhoneNumberSerializer phoneNumberSerializer = new PhoneNumberSerializer();
+
+  @Test
+  void serializeDeserializePhoneNumber() {
+
+    PhoneNumber phoneNumber = PhoneNumber.of(AreaCode.of(408), ExchangeCode.of(555), LineNumber.of(1234));
+
+    phoneNumber.setExtension(Extension.of(9876));
+
+    byte[] data = this.phoneNumberSerializer.serialize(phoneNumber).array();
+
+    assertThat(data).isNotNull().isNotEmpty();
+
+    PhoneNumber deserializedPhoneNumber = this.phoneNumberSerializer.deserialize(ByteBuffer.wrap(data));
+
+    assertThat(deserializedPhoneNumber).isNotNull().isNotSameAs(phoneNumber);
+    assertThat(deserializedPhoneNumber.getAreaCode()).isEqualTo(phoneNumber.getAreaCode());
+    assertThat(deserializedPhoneNumber.getExchangeCode()).isEqualTo(phoneNumber.getExchangeCode());
+    assertThat(deserializedPhoneNumber.getLineNumber()).isEqualTo(phoneNumber.getLineNumber());
+    assertThat(deserializedPhoneNumber.getExtension().orElse(null)).isEqualTo(phoneNumber.getExtension()
+      .orElseGet(() -> Extension.of(123)));
+  }
+
+  @Test
+  void protobufSerializedBytesIsLessThanJavaSerializedBytes() throws IOException {
+
+    PhoneNumber phoneNumber = PhoneNumber.of(AreaCode.of(503), ExchangeCode.of(555), LineNumber.of(1234));
+
+    phoneNumber.setExtension(Extension.of(9876));
+
+    byte[] protobufBytes = this.phoneNumberSerializer.serialize(phoneNumber).array();
+    byte[] javaSerializedBytes = IOUtils.serialize(phoneNumber);
+
+    System.out.printf("Protobuf Serialized bytes [%d] vs. Java Serialized bytes [%d]",
+      protobufBytes.length, javaSerializedBytes.length);
+
+    assertThat(javaSerializedBytes).isNotNull().isNotEmpty();
+    assertThat(protobufBytes).isNotNull().isNotEmpty().hasSizeLessThan(javaSerializedBytes.length);
+  }
+}
